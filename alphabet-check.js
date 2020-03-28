bd0e5e4191d941d8bf95a4c5fd10e41d906092d5
Dawid Adamczyk <39327458+daada95@users.noreply.github.com>
2020-03-28 17:33:50 +0100

    Update alphabet-check.js
    Updated alphabet-check, so that you can start in any moment of the alphabet and still it will return the correct answer.

--- alphabet-check.js
+++ alphabet-check.js
@@ -4,17 +4,27 @@ function alphabetSequence(str) {
   let alphabetArr = "abcdefghijklmnopqrstuvwxyz".split("");
   let strArr = str.split("");
 
-  // we iterate with for loop, checking if letters from alphabet match with letters from input string - if they do in whole string we return undefined, if they don't - we return first letter that doesn't and break the loop
-  for (let i = 0; i < strArr.length; i++) {
-    if (alphabetArr[i] === strArr[i]) {
-      if (i === strArr.length - 1) {
-        return undefined;
-      } else {
-        // nothing to see here, just move on ;)
-      }
-    } else if (alphabetArr[i] !== strArr[i]) {
-      return alphabetArr[i];
-      break;
+  // we check our input to see where we need to start our iteration later on
+  let inputIndexStart = 0;
+  for (let i = 0; i < alphabetArr.length; i++) {
+    if (strArr[0] === alphabetArr[i]) {
+      inputIndexStart = i;
     }
   }
+
+    // we iterate with for loop, checking if letters from alphabet match with letters from input string - if they do in whole string we return undefined, if they don't - we return first letter that doesn't and break the loop
+
+  for (let i = 0; i < strArr.length; i++) {
+    if (strArr[i] === alphabetArr[inputIndexStart + i]) {
+      if (inputIndexStart + i === alphabetArr.length - 1) {
+        return undefined;
+        break;
+      } else {
+        // move on
+      }
+    } else {
+      return alphabetArr[inputIndexStart + i];
+      break;
+    }
+    }
 }
